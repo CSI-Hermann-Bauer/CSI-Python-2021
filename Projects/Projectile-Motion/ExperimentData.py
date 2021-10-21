ae1f86f0b6dc73d69e1af826c36ef5a52f3b395a
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2021-10-21 11:57:38 -0400

    Projectile

--- Projects/Projectile-Motion/ExperimentData.py
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -1,17 +1,9 @@
-class experimentData:
-    #    try:
-    #           def __init__(self, JSON, i):
-    #                  self.gun = JSON['Data'][i]['gun']
-    #                  self.ammo = JSON['Data'][i]['ammo']
-    #                  self.ammo_speed = float(JSON['Data'][i]['ammo_speed'])
-    #                  self.building = JSON['Data'][i]['building']
-    #                  self.building_height_M = float(JSON['Data'][i]['building_height_M'])
-    #                  self.g = float(JSON['Data'][i]['g'])
-    #    except:
-    def __init__(self, angle:float, gun:str, ammo:str, ammo_speed:float, building:str, bulding_height_M:str, g:float):
-        self.gun = gun
-        self.ammo = ammo
-        self.ammo_speed = ammo_speed
-        self.building = building
-        self.building_height_M = bulding_height_M
-        self.g = g
+class ExperimentData:
+
+        def __init__(self, gun:str, ammo:str, ammo_speed:float, building:str, bulding_height_M:float, g:float):
+            self.gun = gun
+            self.ammo = ammo
+            self.ammo_speed = ammo_speed
+            self.building = building
+            self.building_height_M = bulding_height_M
+            self.g = g
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,15 +1,22 @@
 #gun = M700
 #ammo = 7.62x51mm M993
 #building == Popular center 75m
-
 #horizontal disance == Velocity * time
-
 ##t = [V₀ * sin(α) + √((V₀ * sin(α))² + 2 * g * h)] / g
-
 import math
 import json
 from pathlib import Path
-import ExperimentData 
+from ExperimentData import ExperimentData
+import os
+
+
+# gun = "M700"
+# ammo= "7.62x51mm M993"
+# ammo_speed=910
+# building="Popular Center"
+# building_height_M =75
+# g= 9.8
+
 
 # experimetnalData = {"gun": "M700",
 #     "ammo": "7.62x51mm M993",
@@ -18,7 +25,7 @@ import ExperimentData
 #     "building_height_M": 75,
 #     "g": 9.8}
    
-def calcDistance(experimentalData):
+def calcDistance(experimentalData:ExperimentData):
        time_s = math.sqrt((2*experimentalData.building_height_M / experimentalData.g))
        distance_m = experimentalData.ammo_speed * time_s
 
@@ -26,7 +33,20 @@ def calcDistance(experimentalData):
               f"which is {experimentalData.building_height_M} meters at an angle of 0 degrees," +
               f" the bullet will travel {round(time_s, 2)} seconds for a distance of " +
               f"{round(distance_m, 2)} meters \n")
+myDataSet = [
+ExperimentData("M700","7.62x51mm M993",910,"Burj Khalifa",828, 9.8),
+ExperimentData("M700","7.62x51mm M993",910,"Shanghai Tower",632, 9.8),
+ExperimentData("M700","7.62x51mm M993",910,"Lotte World Center",554.5, 9.8),
+ExperimentData("M700","7.62x51mm M993",910,"Bank of America Tower",365.8, 9.8),
+ExperimentData("M700","7.62x51mm M993",910,"Popular Center",75, 9.8)]
 
 
-for i in range(4):
-       calcDistance(ExperimentData.experimentData(json.load(open(Path(__file__).parents[0].joinpath("ExperimentalData.json"))),i))
+for expdata in myDataSet:
+       calcDistance(expdata)
+
+
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'ExperimentDatas.json')
+
+with open(myOutputFilePath, 'w') as outfile:
+       json.dump(expdata[1].__dict__, outfile)
