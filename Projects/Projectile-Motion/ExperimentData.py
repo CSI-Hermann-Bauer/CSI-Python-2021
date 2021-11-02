8698ce0a37ccf168ec13385c0a2af5ed3254bc7f
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2021-11-02 08:45:18 -0400

    Edit class & main

--- Projects/Projectile-Motion/ExperimentData.py
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -1,10 +1,26 @@
+import math
 class ExperimentData:
-
-        def __init__(self, gun:str, ammo:str, ammo_speed:float, building:str, bulding_height_M:float, planet:str, g:float):
-            self.gun = gun
-            self.ammo = ammo
+        def __init__(self, Gunname:str, Guncartridge:str, Round:str, ammo_speed:float, building:str, bulding_height_M:float, planet:str, g:float):
+            self.Gunname = Gunname
+            self.Guncartridge = Guncartridge
+            self.Round = Round
             self.ammo_speed = ammo_speed
             self.building = building
             self.building_height_M = bulding_height_M
             self.planet = planet
-            self.g = g
+        def getTime(self):
+            return math.sqrt(2*self.building_height_M/self.getGravity())
+        
+        def getDistance(self):
+            return (self.ammo_speed * self.getTime())
+        
+        def getGravity(self):
+            planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+            g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+
+            return g_ms2[planets.index(self.planet)]
+
+        def run(self):
+            print(f""" I shot a projectile from {self.building}, its {self.building_height_M} meters tall. 
+            Said projectile was a {self.Guncartridge} fired from a {self.Gunname}. It travelled {self.getDistance()} 
+            at {self.ammo_speed} m/s^2 for {self.getTime():.2}. It was fired at {self.planet} where the gravity is {self.getGravity()}""" )
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -26,31 +26,31 @@ import os
 #     "g": 9.8}
  # Parallel Lists
 
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
 
-# Planet Index
-planet = input("Planet Name: ")
-planetIndex = int(planets.index(planet))
+# # Planet Index
+# planet = input("Planet Name: ")
+# planetIndex = int(planets.index(planet))
 
-def calcDistance(experimentalData:ExperimentData, g:float):
-       time_s = math.sqrt((2*experimentalData.building_height_M / g))
-       distance_m = experimentalData.ammo_speed * time_s
+# def calcDistance(experimentalData:ExperimentData, g:float):
+#        time_s = math.sqrt((2*experimentalData.building_height_M / g))
+#        distance_m = experimentalData.ammo_speed * time_s
 
-       print(f"If you fire an {experimentalData.gun} using {experimentalData.ammo} bullets from the top of {experimentalData.building} " +
-              f"which is {experimentalData.building_height_M} meters at an angle of 0 degrees," +
-              f" the bullet will travel {round(time_s, 2)} seconds for a distance of " +
-              f"{round(distance_m, 2)} meters \n")
+#        print(f"If you fire an {experimentalData.gun} using {experimentalData.ammo} bullets from the top of {experimentalData.building} " +
+#               f"which is {experimentalData.building_height_M} meters at an angle of 0 degrees," +
+#               f" the bullet will travel {round(time_s, 2)} seconds for a distance of " +
+#               f"{round(distance_m, 2)} meters \n")
 myDataSet = [
-ExperimentData("M700","7.62x51mm M993",910,"Burj Khalifa",828, 9.8, planet),
-ExperimentData("M700","7.62x51mm M993",910,"Shanghai Tower",632, 9.8, planet),
-ExperimentData("M700","7.62x51mm M993",910,"Lotte World Center",554.5, 9.8, planet),
-ExperimentData("M700","7.62x51mm M993",910,"Bank of America Tower",365.8, 9.8, planet),
-ExperimentData("M700","7.62x51mm M993",910,"Popular Center",75, 9.8, planet)]
+ExperimentData("HK 416A5","5.56x45mm NATO", "5.56x45 HP", 947,"Burj Khalifa",828, "Earth"),
+ExperimentData("HK 416A5","5.56x45mm NATO", "5.56x45 HP", 947,"Burj Khalifa",828, "Mars"),
+ExperimentData("HK 416A5","5.56x45mm NATO", "5.56x45 HP", 947,"Burj Khalifa",828, "Mercury"),
+ExperimentData("HK 416A5","5.56x45mm NATO", "5.56x45 HP", 947,"Burj Khalifa",828, "Venus"),
+ExperimentData("HK 416A5","5.56x45mm NATO", "5.56x45 HP", 947,"Burj Khalifa",828, "Jupiter")]
 
 
-for expdata in myDataSet:
-       calcDistance(expdata, float(g_ms2[planetIndex]))
+# for expdata in myDataSet:
+#        calcDistance(expdata, float(g_ms2[planetIndex]))
 
 
 myOutputPath = Path(__file__).parents[0]
@@ -59,10 +59,8 @@ myOutputFilePath = os.path.join(myOutputPath, 'ExperimentDatas.json')
 with open(myOutputFilePath, 'w') as outfile:
        json.dump(myDataSet[1].__dict__, outfile)
 
+deserialize = open(myOutputFilePath)
+experimentJson = json.loads(deserialize)
 
-# deserialize = open(myOutputFilePath)
-
-# experimentJson = json.loads(deserialize)
-
-# for e in experimentJson:
-#        ExperimentData(**e).run()
+for e in experimentJson:
+       ExperimentData(**e).run()
