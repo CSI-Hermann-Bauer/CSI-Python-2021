cb6ad97a0646bae986ddeb82ceadb1cfe0d912ab
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-01-27 11:45:53 -0400

    Random_beer
    Random_beer

---
+++ Projects/Web-Servers/FileDeserialization.py
@@ -0,0 +1,29 @@
+import json
+import os
+from pathlib import Path
+from beer import Beer
+
+#  Locate and open file
+myPath = Path(__file__).parents[0]
+myFilePath = os.path.join(myPath, 'random_beer.json')
+#print(myFilePath)
+data = open(myFilePath, 'r')
+ 
+# deserializing the data
+data = json.load(data)
+beer:Beer = Beer(**data)
+
+# Print data from the object
+print(f"ID: {beer.id}")
+print(f"UID: {beer.uid}")
+print(f"Brand: {beer.brand}")
+print(f"Name: {beer.name}")
+print(f"Style: {beer.style}")
+print(f"Hop: {beer.hop}")
+print(f"Yeast: {beer.yeast}")
+print(f"Malts: {beer.malts}")
+print(f"Ibu: {beer.ibu}")
+print(f"Alcohol: {beer.alcohol}")
+print(f"Blg: {beer.blg}")
+print("Others expected...")
+
