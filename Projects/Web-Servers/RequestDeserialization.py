43d415fff434993abd986e4758c917ae94f05dab
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-02-04 12:00:55 -0400

    Update RequestDeserialization.py

--- Projects/Web-Servers/RequestDeserialization.py
+++ Projects/Web-Servers/RequestDeserialization.py
@@ -14,16 +14,15 @@ myFolderPath = os.path.join(myPath, 'responses')
 os.makedirs(myFolderPath)
 
 # This is the URL from which we will request the data
-addressURL = "https://random-data-api.com/api/beer/random_beer?size=100"
+beerURL = "https://random-data-api.com/api/beer/random_beer?size=100"
 
 # Create a list to populate with our data
 beers:Beer = [] 
 
-
 # Loop over JSON items and Deserialize them into python objects
 for i in range(100):
     #make the request
-    req = urllib.request.Request(addressURL)
+    req = urllib.request.Request(beerURL)
     requestData = json.loads(urllib.request.urlopen(req).read())
     for r in requestData:  
         # Deserialize 
