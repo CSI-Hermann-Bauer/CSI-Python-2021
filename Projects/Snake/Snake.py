f47cce2b7e48ffe7c5470a87bd1e604d5312173c
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-03-09 11:30:13 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 #initialize imported pygame
 pygame.init()
@@ -8,8 +9,20 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+#define dimentions
+dis_width = 800
+dis_height  = 600
+
 #sets window size
-dis=pygame.display.set_mode((800,600))
+dis = pygame.display.set_mode((dis_width, dis_width))
+
+#define x1 and x 2 as half of the width and len
+x1 = dis_width/2
+y1 = dis_height/2
+ 
+#set size for snake block
+snake_block=10
+
 #updates display
 pygame.display.update()
 #sets caption
@@ -26,6 +39,16 @@ y1_change = 0
 
 #clock to keep time
 clock = pygame.time.Clock()
+#set speed for snake
+snake_speed = 30
+
+#set font
+font_style = pygame.font.SysFont(None, 50)
+ 
+ #output message
+def message(msg,color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
 
 #while loop to keep window open
 while not game_over:
@@ -37,29 +60,39 @@ while not game_over:
         #If key is pushed, update the coordinates accordint to key pushed
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
+                x1_change = -snake_block
                 y1_change = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
+                x1_change = snake_block
                 y1_change = 0
             elif event.key == pygame.K_UP:
-                y1_change = -10
+                y1_change = -snake_block
                 x1_change = 0
             elif event.key == pygame.K_DOWN:
-                y1_change = 10
+                y1_change = snake_block
                 x1_change = 0
+
+#chec if snake leaves and if so end game
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
     #update coordinates by adding the change
- 
     x1 += x1_change
     y1 += y1_change
-    #fill with white
+    #fill background  with white
     dis.fill(white)
     #add a new rectangle
     pygame.draw.rect(dis, black, [x1, y1, 10, 10])
     #update display
     pygame.display.update()
     #sleep for a while
-    clock.tick(30)
+    clock.tick(snake_speed)
+
+#print you lost
+message("You lost",red)
+#umpdate display
+pygame.display.update()
+#sleep
+time.sleep(2)
 #clsoes display
 pygame.quit()
 #quits pygame
