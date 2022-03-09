840164e7f41c6ff5bc371d73c088ec83ab22f1c6
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-03-09 11:49:18 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -7,12 +7,15 @@ pygame.init()
 
 #define colors
 white = (255, 255, 255)
+yellow = (255, 255, 102)
 black = (0, 0, 0)
-red = (255, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
 
 #define dimentions
-dis_width = 800
-dis_height  = 600
+dis_width = 600
+dis_height  = 400
 
 #sets window size
 dis = pygame.display.set_mode((dis_width, dis_width))
@@ -37,11 +40,19 @@ game_over=False
 
 #clock to keep time
 clock = pygame.time.Clock()
+ #set snake block sie
+snake_block = 10
 #set speed for snake
-snake_speed = 30
+snake_speed = 15
 
-#set font
-font_style = pygame.font.SysFont(None, 50)
+#set fonts
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+
+ #funstion for each snake block in the snake to follow list of instructions
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
  
  #output message
 def message(msg,color):
@@ -58,6 +69,10 @@ def gameLoop():
     x1_change = 0       
     y1_change = 0
 
+    #define snake list and lenght
+    snake_List = []
+    Length_of_snake = 1
+
     #add food location variables at random 
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
@@ -67,7 +82,7 @@ def gameLoop():
         #WHAT TO DO IF CLOSE   
         while game_close == True:
             #background white
-            dis.fill(white)
+            dis.fill(blue)
             #print You lost
             message("You Lost! Press Q-Quit or C-Play Again", red)
             #update display
@@ -101,11 +116,35 @@ def gameLoop():
         #loose if hit border
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
-        #change variables
+        #change coordinate of snake head
         x1 += x1_change
         y1 += y1_change
-        #fill background white
-        dis.fill(white)
+        #fill blue
+        dis.fill(blue)
+        #draw  rectangle
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        #snake head defined
+        snake_Head = []
+        #append coordinates to snake head
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        #append snake head location to snake_List
+        snake_List.append(snake_Head)
+        #delete first object on snake_List
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+        
+        #check if head crosses body and loose
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+        #call function to update location of all the squares
+        our_snake(snake_block, snake_List)
+
+
+        #update display
+
+        pygame.display.update()
         #add food
         pygame.draw.rect(dis, "blue", [foodx, foody, snake_block, snake_block])
         pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
@@ -113,7 +152,11 @@ def gameLoop():
         pygame.display.update()
         #print "yummy" if snake over food
         if x1 == foodx and y1 == foody:
-            print("Yummy!!")
+            #set new food location
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            #increase the length of the snake by one square
+            Length_of_snake += 1
         #snake speed
         clock.tick(snake_speed)
  
