afa9f51133d9e501fc9b46969e2eb5c3c6daa5b2
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-03-09 11:16:12 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -6,6 +6,15 @@ pygame.init()
 dis=pygame.display.set_mode((400,300))
 #updates display
 pygame.display.update()
+#sets caption
+pygame.display.set_caption('Snake game by Edureka')
+#variable used in while loop 
+game_over=False
+
+#while loop to keep window open
+while not game_over:
+    for event in pygame.event.get():
+        print(event) 
 #clsoes display
 pygame.quit()
 #quits pygame
