c6a4db78cccf1364ec52aa2bb2a5eec52dfcd153
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-03-09 11:14:02 -0400

    Create Snake.py

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,12 @@
+import pygame
+
+#initialize imported pygame
+pygame.init()
+#sets window size
+dis=pygame.display.set_mode((400,300))
+#updates display
+pygame.display.update()
+#clsoes display
+pygame.quit()
+#quits pygame
+quit()
