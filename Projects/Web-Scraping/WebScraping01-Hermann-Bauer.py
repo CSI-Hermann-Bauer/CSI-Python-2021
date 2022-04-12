8eb204acc9e3ea92a650e013419b255bc33df7e2
CSI-Hermann-Bauer <89210478+CSI-Hermann-Bauer@users.noreply.github.com>
2022-04-11 23:18:18 -0400

    mnm

--- Projects/Web-Scraping/WebScraping01-Hermann-Bauer.py
+++ Projects/Web-Scraping/WebScraping01-Hermann-Bauer.py
@@ -2,6 +2,6 @@ import chunk
 import requests
 res = requests.get("https://www.gutenberg.org/cache/epub/67775/pg67775.txt")
 playFile = open("Gaudenzia", "wb")
-for char in res.text:
-    playFile.write(chunk)
+for char in res.iter_content(100001):
+    playFile.write(char)
 playFile.close
